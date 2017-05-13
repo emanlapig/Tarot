d609bebd61a8c50119d5c63aeddce4bb8f36a79d
emanlapig <emmanlapig@gmail.com>
2017-05-13 12:32:35 -0400

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'css/style.css' : 'css/style.scss'
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'css/style.min.css': ['css/style.css']
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'js/min/Main.min.js' : 'js/Main.js',
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['css/*.scss', 'js/*.js'],
+        tasks: ['sass', 'cssmin', 'uglify']
+      }
+    }
+  });
+
+  // Load the plugin(s).
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'cssmin', 'uglify', 'watch']);
+
+};
